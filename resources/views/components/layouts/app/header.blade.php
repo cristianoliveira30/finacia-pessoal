4d1e747103b520025274edd8983f139d4f0a9751
natamaia <natamaia101@gmail.com>
2025-12-02 18:57:30 -0300

    logo

--- resources/views/components/layouts/app/header.blade.php
+++ resources/views/components/layouts/app/header.blade.php
@@ -13,8 +13,8 @@
                     </svg>
                 </button>
                 <a href="#" class="flex ms-2 md:me-24">
-                    <img src="https://flowbite.com/docs/images/logo.svg" class="h-6 me-3" alt="FlowBite Logo" />
-                    <span class="self-center text-lg font-semibold whitespace-nowrap dark:text-white">Flowbite</span>
+                    <img src="{{ asset('assets/img/para.png') }}" class="h-6 me-3" alt="pará Logo" />
+                    <span class="self-center text-lg font-semibold whitespace-nowrap dark:text-white">Core</span>
                 </a>
             </div>
             <div class="flex items-center">
