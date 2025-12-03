d77473bec71775c4b6464386764d52919cdf5a23
natamaia <natamaia101@gmail.com>
2025-12-03 16:58:19 -0300

    layout

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,28 +5,28 @@
     {{-- 3. padding-top mantido para não ficar atrás do header. --}}
     
     <!-- card exemplo -->
-    <div class="grid grid-cols-4 gap-3">
+    <div class="grid grid-cols-5 md:grid-cols-4 gap-3 justify-items-center">
         <div class="col p-2">
-            <div class="bg-neutral-primary-soft block max-w-sm p-6 border border-default rounded-base shadow-xs">
-                <h5 class="mb-2 text-2xl font-bold tracking-tight text-heading dark:text-slate-100">Welcome to the Dashboard</h5>
+            <div class="dark:bg-indigo-500 dark:border-gray-900 bg-sky-200 block max-w-xs p-6 border border-default rounded-base shadow-xs">
+                <h5 class="mb-2 text-l font-bold tracking-tight text-heading dark:text-slate-100">Welcome to the Dashboard</h5>
                 <p class="font-normal text-body dark:text-slate-300">This is your main dashboard where you can find an overview of your activities and statistics.</p>
             </div>
         </div>
         <div class="col p-2">
-            <div class="bg-neutral-primary-soft block max-w-sm p-6 border border-default rounded-base shadow-xs">
-                <h5 class="mb-2 text-2xl font-bold tracking-tight text-heading dark:text-slate-100">Welcome to the Dashboard</h5>
+            <div class="dark:bg-indigo-500 dark:border-gray-900 bg-sky-200 block max-w-xs p-6 border border-default rounded-base shadow-xs">
+                <h5 class="mb-2 text-l font-bold tracking-tight text-heading dark:text-slate-100">Welcome to the Dashboard</h5>
                 <p class="font-normal text-body dark:text-slate-300">This is your main dashboard where you can find an overview of your activities and statistics.</p>
             </div>
         </div>
         <div class="col p-2">
-            <div class="bg-neutral-primary-soft block max-w-sm p-6 border border-default rounded-base shadow-xs">
-                <h5 class="mb-2 text-2xl font-bold tracking-tight text-heading dark:text-slate-100">Welcome to the Dashboard</h5>
+            <div class="dark:bg-indigo-500 dark:border-gray-900 bg-sky-200 block max-w-xs p-6 border border-default rounded-base shadow-xs">
+                <h5 class="mb-2 text-l font-bold tracking-tight text-heading dark:text-slate-100">Welcome to the Dashboard</h5>
                 <p class="font-normal text-body dark:text-slate-300">This is your main dashboard where you can find an overview of your activities and statistics.</p>
             </div>
         </div>
         <div class="col p-2">
-            <div class="bg-neutral-primary-soft block max-w-sm p-6 border border-default rounded-base shadow-xs">
-                <h5 class="mb-2 text-2xl font-bold tracking-tight text-heading dark:text-slate-100">Welcome to the Dashboard</h5>
+            <div class="dark:bg-indigo-500 dark:border-gray-900 bg-sky-200 block max-w-xs p-6 border border-default rounded-base shadow-xs">
+                <h5 class="mb-2 text-l font-bold tracking-tight text-heading dark:text-slate-100">Welcome to the Dashboard</h5>
                 <p class="font-normal text-body dark:text-slate-300">This is your main dashboard where you can find an overview of your activities and statistics.</p>
             </div>
         </div>
