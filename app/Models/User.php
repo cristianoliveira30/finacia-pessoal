e1fb36cf413bb511f88b63a41559ee2d4906ae08
MarcusCalderaro <marcuseduardo54@gmail.com>
2026-01-17 10:29:04 -0300

    feat: arquitetura de departamentos e permissões

--- app/Models/User.php
+++ app/Models/User.php
@@ -49,6 +49,11 @@ class User extends Authenticatable
         ];
     }
 
+    public function department()
+    {
+        return $this->belongsTo(Department::class, 'id', 'department_id');
+    }
+
     protected static function rules(): array
     {
         return [
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,8 @@
 
 namespace Database\Seeders;
 
+use App\Models\Department;
+use App\Models\Role;
 use App\Models\User;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
@@ -16,40 +18,86 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
+        // Antes de tudo, vamos criar os departamentos e permissões
+        $departments = [
+            ['name' => 'Administração'],
+            ['name' => 'Financeiro'],
+            ['name' => 'Educação'],
+            ['name' => 'Saúde']
+        ];
 
+        foreach ($departments as $department) {
+            Department::create($department);
+        }
+
+        $roles = [
+            ['name' => 'Admin', 'description' => 'Administrador do sistema', 'level' => 100],
+            ['name' => 'Manager', 'description' => 'Gerente de departamento', 'level' => 90],
+            ['name' => 'Staff', 'description' => 'Funcionário regular', 'level' => 50],
+            ['name' => 'Intern', 'description' => 'Estagiário', 'level' => 10],
+            ['name' => 'Guest', 'description' => 'Usuário convidado', 'level' => 1],
+        ];
+
+        foreach ($roles as $role) {
+            Role::create($role);
+        }
+
+        // Vamos separar o departamento e a permissão padrão para os novos usuários
+        $defaultDepartment = Department::where('name', 'Administração')->first();
+        $defaultRole = Role::where('name', 'Admin')->first();
+
+        // Create default users
         $users = [
             [
                 'name' => 'Marcus Calderaro',
                 'username' => 'marcus.calderaro',
                 'email' => 'marcuseduardo54@gmail.com',
-                'password' => Hash::make('321654987')
+                'password' => Hash::make('321654987'),
+                'department_id' => $defaultDepartment->id,
+                'role_id' => $defaultRole->id
             ],
             [
                 'name' => 'Celso Castro',
                 'username' => 'celso.castro',
                 'email' => 'celsoos@hotmail.com',
-                'password' => Hash::make('12345678')
+                'password' => Hash::make('12345678'),
+                'department_id' => $defaultDepartment->id,
+                'role_id' => $defaultRole->id
             ],
             [
                 'name' => 'Cristian Roan',
                 'username' => 'cristian.roan',
                 'email' => 'cristianroan30@gmail.com',
-                'password' => Hash::make('87654321')
+                'password' => Hash::make('87654321'),
+                'department_id' => $defaultDepartment->id,
+                'role_id' => $defaultRole->id
             ],
             [
                 'name' => 'Renan Cardoso',
                 'username' => 'renan.cardoso',
                 'email' => 'superrenanxxx@gmail.com',
-                'password' => Hash::make('244466666')
+                'password' => Hash::make('244466666'),
+                'department_id' => $defaultDepartment->id,
+                'role_id' => $defaultRole->id
             ],
             [
                 'name' => 'Natanael Maia',
                 'username' => 'nata.maia',
                 'email' => 'natamaia101@gmail.com',
-                'password' => Hash::make('12345678')
+                'password' => Hash::make('12345678'),
+                'department_id' => $defaultDepartment->id,
+                'role_id' => $defaultRole->id
+            ],
+            [
+                'name' => 'Mateus Farias',
+                'username' => 'mateus.farias',
+                'email' => 'mateus.sarges99@gmail.com',
+                'password' => Hash::make('12345678'),
+                'department_id' => $defaultDepartment->id,
+                'role_id' => $defaultRole->id
             ]
         ];
+
         foreach ($users as $user) {
             User::create($user);
         }
