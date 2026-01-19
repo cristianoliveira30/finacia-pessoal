da1d1d18a4e813e96af8abae17667d20695d6929
natamaia <natamaia101@gmail.com>
2026-01-19 16:55:43 -0300

    fix:erro simples de codigo

--- resources/views/educacao/relatorios/infraestrutura.blade.php
+++ resources/views/educacao/relatorios/infraestrutura.blade.php
@@ -1,4 +1,4 @@
-<x-layouts.report :title="$tableConfig['title'] ?? 'relatório de infraestrutura'>"
+<x-layouts.report :title="$tableConfig['title'] ?? 'relatório de infraestrutura'">
     @php
         $tableConfig = [
             'id' => 'table-infra',
@@ -11,10 +11,31 @@
                 ['key' => 'capacity', 'label' => 'Capacidade (Alunos)', 'align' => 'center'],
             ],
             'rows' => [
-                ['school_code' => 'PRED-01', 'school_name' => 'E.M. Monteiro Lobato', 'condition' => 'Bom', 'last_maintenance' => '10/01/2024', 'maintenance_cost' => 15000.00, 'capacity' => 500],
-                ['school_code' => 'PRED-05', 'school_name' => 'Quadra Poliesportiva Central', 'condition' => 'Precisa de Reparos', 'last_maintenance' => '15/05/2022', 'maintenance_cost' => 0.00, 'capacity' => 0],
-                ['school_code' => 'PRED-08', 'school_name' => 'C.M.EI. Pingo de Gente', 'condition' => 'Ótimo', 'last_maintenance' => '20/08/2024', 'maintenance_cost' => 5400.00, 'capacity' => 120],
-            ]
+                [
+                    'school_code' => 'PRED-01',
+                    'school_name' => 'E.M. Monteiro Lobato',
+                    'condition' => 'Bom',
+                    'last_maintenance' => '10/01/2024',
+                    'maintenance_cost' => 15000.0,
+                    'capacity' => 500,
+                ],
+                [
+                    'school_code' => 'PRED-05',
+                    'school_name' => 'Quadra Poliesportiva Central',
+                    'condition' => 'Precisa de Reparos',
+                    'last_maintenance' => '15/05/2022',
+                    'maintenance_cost' => 0.0,
+                    'capacity' => 0,
+                ],
+                [
+                    'school_code' => 'PRED-08',
+                    'school_name' => 'C.M.EI. Pingo de Gente',
+                    'condition' => 'Ótimo',
+                    'last_maintenance' => '20/08/2024',
+                    'maintenance_cost' => 5400.0,
+                    'capacity' => 120,
+                ],
+            ],
         ];
     @endphp
     <x-export-table :config="$tableConfig" />
