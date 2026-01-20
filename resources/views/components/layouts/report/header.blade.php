8e7d032741e3c9b51cccc0d641b8d25afc604c22
natamaia <natamaia101@gmail.com>
2026-01-20 12:27:00 -0300

    fix:fechamento da div e borda no header relatorio

--- resources/views/components/layouts/report/header.blade.php
+++ resources/views/components/layouts/report/header.blade.php
@@ -1,8 +1,8 @@
-<nav class="relative top-0 z-40 w-full bg-neutral-900 dark:bg-neutral-900 border-b border-default">
+<nav class="relative top-0 z-40 w-full bg-neutral-900 dark:bg-neutral-900">
     <div class="px-3 py-3 lg:px-5 lg:pl-3">
         <div class="flex items-center justify-between">
             <div class="flex items-center justify-start rtl:justify-end">
-                <a href="{{ route('home')}}" class="flex ms-2 md:me-6 me-2">
+                <a href="{{ route('home') }}" class="flex ms-2 md:me-6 me-2">
                     <img src="{{ asset('assets/img/belem.png') }}" class="h-6 me-3" alt="pará Logo" />
                     <span
                         class="self-center text-lg font-semibold whitespace-nowrap text-white dark:text-white">Bi4gov</span>
@@ -16,8 +16,8 @@
                             Relatório de Eleitores - Mês Passado
                         </h2>
                     </div>
+                </div>
             </div>
-
         </div>
     </div>
 </nav>
