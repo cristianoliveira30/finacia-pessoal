3609b121f101056b31f9ff2efe4cce62bb726257
natamaia <natamaia101@gmail.com>
2026-01-20 11:59:21 -0300

    fix:orientação do box

--- resources/views/errors/nodata.blade.php
+++ resources/views/errors/nodata.blade.php
@@ -1,7 +1,5 @@
 <x-layouts.app title="{{ $title ?? 'Sem dados' }}">
-    <div class="w-full h-screen flex flex-col items-center justify-center
-                px-6">
-
+    <div class="w-full min-h-[calc(100vh-4rem)] flex flex-col items-center justify-center px-6">
         {{-- CARD DE MENSAGEM --}}
         <div
             class="w-full max-w-md p-8 text-center rounded-2xl border border-solid transition-colors
@@ -9,13 +7,11 @@
                     dark:bg-slate-800/50 dark:border-slate-700
                     black:bg-zinc-900 black:border-zinc-800">
 
-            {{-- ÍCONE NEUTRO --}}
             <div
                 class="mx-auto mb-4 flex h-18 w-18 items-center justify-center rounded-full
                         bg-slate-100 text-slate-400
                         dark:bg-slate-700 dark:text-slate-400
                         black:bg-zinc-800 black:text-zinc-600">
-                {{-- Use 'bi-cone-striped' para construção ou 'bi-inbox' para sem dados --}}
                 <x-bi-cone-striped class="h-6 w-6" />
             </div>
 
